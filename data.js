8724683f2b171a6e91326d0f5d64fc9f9aeac8ce
alex koffi 2004 <kouadiojeanalexkoffi@gmail.com>
2025-02-08 12:26:10 +0000

    feat

---
+++ data.js
@@ -0,0 +1,72 @@
+[
+    { 
+        title: "One Hundred Years of Solitude",
+         author: "Gabriel García Márquez", 
+         year: 1967, 
+         available: true, 
+         genres: ["Fiction", "Magical Realism"] 
+        },
+    { 
+        title: "Chronicle of a Death Foretold", 
+        author: "Gabriel García Márquez", 
+        year: 1981, 
+        available: false, 
+        genres: ["Fiction", "Mystery"] 
+    },
+    { 
+        title: "Love in the Time of Cholera", 
+        author: "Gabriel García Márquez", 
+        year: 1985, 
+        available: true, 
+        genres: ["Fiction", "Romance"] 
+    },
+    { 
+        title: "IT", 
+        author: "Stephen King", 
+        year: 1986, 
+        available: true, 
+        genres: ["Fiction", "Horror"] 
+    },
+    { 
+        title: "The Shining", 
+        author: "Stephen King", 
+        year: 1977, 
+        available: false, 
+        genres: ["Fiction", "Horror"] 
+    },
+    { 
+        title: "Carrie", 
+        author: "Stephen King", 
+        year: 1974, 
+        available: true, 
+        genres: ["Fiction", "Horror"] 
+    },
+    { 
+        title: "Vertigo", 
+        author: "Ahmed Mourad", 
+        year: 2007, 
+        available: true, 
+        genres: ["Fiction", "Thriller"] 
+    },
+    { 
+        tile: "The Blue Elephant", 
+        author: "Ahmed Mourad", 
+        year: 2012, 
+        available: true, 
+        genres: ["Fiction", "Psychological Thriller"] 
+    },
+    { 
+        title: "1919", 
+        author: "Ahmed Mourad", 
+        year: 2010, 
+        available: false, 
+        genres: ["Fiction", "Historical"] 
+    },
+    { 
+        title: "A Murder Is Announced", 
+        author: "Ahmed Mourad", 
+        year: 2018, 
+        available: true, 
+        genres: ["Fiction", "Mystery"]
+     }
+]
